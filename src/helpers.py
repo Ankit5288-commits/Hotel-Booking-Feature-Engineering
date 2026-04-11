e7664cf837e3e2367e9cdae801e6813aade8f5df
Ankit5288-commits <hardcoreprogrammer0101bit@gmail.com>
2026-04-11 15:40:21 +0530

    Implement numerical preprocessing pipeline
    Added a function to build a numerical preprocessing pipeline.

--- src/helpers.py
+++ src/helpers.py
@@ -1,1 +1,10 @@
-Helper functions file
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import PowerTransformer, StandardScaler
+
+def build_num_pipeline(numeric_features):
+    numeric_transformer = Pipeline(steps=[ ('imputer', SimpleImputer(strategy='median')),('power', PowerTransformer(method='yeo-johnson')), ('scaler', StandardScaler())])
+    
+    preprocessor = ColumnTransformer(transformers=[('num', numeric_transformer, numeric_features)])
+    return preprocessor
